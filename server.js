2a8597cea2d78b0196cac964230efdedab63663d
callmecposu <pc.cposu@gmail.com>
2023-11-03 13:48:22 -0400

    update for A4

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
 /***************************************************************************
-* BTI325 - Assignment 03
+* BTI325 - Assignment 04
 *
 米
 * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 * https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 *
-* Name: Maksym Volkovynskyi   Student ID: 126867225   Date: 2023/10/15
+* Name: Maksym Volkovynskyi   Student ID: 126867225   Date: 2023/11/03
 *
 * Published URL: https://sleepy-purse-bat.cyclic.app/
 ***************************************************************************/
 
-
 const legoData = require("./modules/legoSets");
-
-const path = require("path");
 
 const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
+  res.render('home')
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/about.html"));
+  res.render('about');
 });
 
 app.get("/lego/sets", (req, res) => {
   if (req.query.theme) {
     legoData
       .getSetsbyTheme(req.query.theme)
-      .then((result) => res.json(result))
-      .catch((err) => res.status(404).send(err));
+      .then((result) => res.render('sets', {sets: result, theme: req.query.theme}))
+      .catch((err) => res.status(404).render('404', {message: err}));
   } else {
-    legoData.getAllSets().then((result) => res.json(result));
+    legoData.getAllSets().then((result) => res.render('sets', {sets: result, theme: ''}))
   }
 });
 
 app.get("/lego/sets/:setNum", (req, res) => {
   legoData
     .getSetsByNum(req.params.setNum)
-    .then((sets) => res.json(sets))
-    .catch((err) => res.status(404).send(err));
+    .then((sets) => res.render('set', {set:sets}))
+    .catch((err) => res.status(404).render('404', {message: err}));
 });
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'))
-})
+  res.status(404).render('404', {message: 'Path Not Found :c'})
+});
 
 legoData.initialize().then(() => {
   app.listen(3001, () => {
